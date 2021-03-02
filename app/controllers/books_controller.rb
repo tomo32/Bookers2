4c4b9985e5ca07a86149716151c44af40b682823
tomo32 <19da018k@rikkyo.ac.jp>
2021-03-02 15:13:57 +0000

    finished settings

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,14 @@
+class BooksController < ApplicationController
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to:'homes#top'
+  devise_for :users
+  resources :books, only: [:create, :index, :show, :destroy]
+  get "/users" => "homes#users", as: 'users_path'
+  get "/home/about" => "homes#about", as: 'about_path'
+end
